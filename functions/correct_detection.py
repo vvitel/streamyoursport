2ebe9242f62d0211372c9802870d18ffcd7c0a1d
vvitel <109207573+vvitel@users.noreply.github.com>
2026-06-15 15:24:47 +0200

    ajout correction des détections

---
+++ functions/correct_detection.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def correct_detection(frame_list):
+    isolated_points = []
+    for i in range(1, len(frame_list)-1):
+        x_before, y_before = frame_list[i-1][0], frame_list[i-1][1]
+        x, y = frame_list[i][0], frame_list[i][1]
+        x_after, y_after = frame_list[i+1][0], frame_list[i+1][1]
+        
+        distance1 = np.sqrt((x - x_before)**2 + (y - y_before)**2)
+        distance2 = np.sqrt((x - x_after)**2 + (y - y_after)**2)
+
+        if (distance1 > 5 and distance2 > 5) or abs(distance1 - distance2) > 3:
+            isolated_points.append(i)
+
+    clean_list = [p for i, p in enumerate(frame_list) if i not in isolated_points]
+    return clean_list
